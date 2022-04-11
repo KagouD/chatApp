bb6780f8bfe5cb96dd1791356195387698586c78
Kagou Dia <kagoudia@MacBook-Pro-de-Kagou-Dia.local>
2022-04-11 23:59:05 +0200

    initial commit

---
+++ src/DirectMessaging.js
@@ -0,0 +1,49 @@
+// DirectMessaging.js file code
+import React, {useState} from 'react';
+import {ChatEngine, getOrCreateChat} from 'react-chat-engine';
+
+const DirectMessaging = () => {
+    // The useState hook initially sets the username to an empty string
+    const[username, setUsername] = useState('')
+    //Custom function that will implement the getOrCreateChat function that to select username to chat with
+    //only when the correct credentials(user  secret, project id, username) are passed will the application be rendered
+    function implementingDirectChat(credentials){
+        getOrCreateChat(
+            credentials,
+            {is_direct_chat: true, usernames:[username]},
+            () => setUsername('')
+        )
+    }
+
+    const displayChatInterface = (credentials) => {
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder='Find username'
+                    value={username} 
+                    // A controlled function that sets the username to what the user types in the input field
+                    onChange = {(e) => setUsername(e.target.value)}
+                />
+
+                {/* clicking button will call the implementingDirectChat function that displays a list of usernames to create or find an existing chat.  */}
+                <button onClick={() => implementingDirectChat(credentials)}>
+                    Create Chat
+                </button>
+
+            </div>
+        )
+    }
+
+    return(
+        <ChatEngine
+            height='100vh'
+            userName='kagou'
+            userSecret='kagou'
+            projectID='b041076f-a2c0-42ac-9258-ae2696166bda'
+            displayNewChatInterface={(credentials) => displayChatInterface(credentials)}
+            />
+    );
+}
+
+export default DirectMessaging
